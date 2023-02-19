32d5bf59bb15583a59e4da1b2fa7fe01cecb3aee
carlos8788 <carlos8788@gmail.com>
2023-02-19 16:28:19 -0300

    deploy

--- api_arduino/settings.py
+++ api_arduino/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 # DEBUG = 'RENDER' not in os.environ
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['apiarduino-production.up.railway.app']
 
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 # if RENDER_EXTERNAL_HOSTNAME:
